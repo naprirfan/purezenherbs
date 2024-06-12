b2cbeecb2adb9577ada1da8c467256f5779d37ed
Muhammad Irfan <naprirfan@gmail.com>
2024-06-12 12:57:25 +0800

    Fix lint

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,11 @@
 
 export const Header = () => {
-    return (
-        <header className="w-full">
-            <section className="bg-charcoal text-white text-center p-1">Selamat datang di situs Pure Zen Herbs</section>
-            <section className="h-12 bg-white border-b-charcoal">
+  return (
+    <header className="w-full">
+      <section className="bg-charcoal text-white text-center p-1">Selamat datang di situs Pure Zen Herbs</section>
+      <section className="h-12 bg-white border-b-charcoal">
                 Hello world
-            </section>
-        </header>
-    )
+      </section>
+    </header>
+  )
 }
