b71bd2641cbd01bfbf1181879b193c3204ba162d
Muhammad Irfan <naprirfan@gmail.com>
2024-06-12 13:31:03 +0800

    Add header layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,13 @@
-
 export const Header = () => {
   return (
     <header className="w-full">
       <section className="bg-charcoal text-white text-center p-1">Selamat datang di situs Pure Zen Herbs</section>
-      <section className="h-12 bg-white border-b-charcoal">
-                Hello world
+      <section className="h-12 bg-white border-b flex justify-center">
+        <div className="container px-2 flex justify-between">
+            <div>About Us</div>
+            <div>Logo</div>
+            <div>Language Control</div>
+        </div>
       </section>
     </header>
   )
