725a9278cbc9e3e3019895277354c891036ffc61
Muhammad Irfan <naprirfan@gmail.com>
2024-06-12 19:06:07 +0800

    Fix style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
     <header className="w-full">
       <section className="bg-charcoal text-white text-center p-1">Selamat datang di situs Pure Zen Herbs</section>
       <section className="h-12 border-b flex justify-center bg-gray">
-        <div className="container px-2 flex justify-between">
+        <div className="container px-2 flex justify-between items-center">
             <div>Shop | About Us</div>
             <div>Logo</div>
             <div>Language Control</div>
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,14 @@
 export const Hero = () => {
     return (
         <div className="bg-gray h-[750px] w-full border-b flex justify-center">
-            <div className="container flex justify-between px-2">
-                <div>
-                    <h1 className="text-4xl text-bold">Where Traditional Wisdom Meets Convenience</h1>
+            <div className="container flex flex-row px-2">
+                <div className="basis-1/2 flex items-center">
+                    <div>
+                        <h1 className="text-6xl p-10 text-bold">Where Traditional Wisdom Meets Convenience</h1>
+                        <button className="text-white border-2 bg-charcoal px-4 py-2 mt-4">Belanja Sekarang</button>
+                    </div>
                 </div>
-                <div>Image here</div>
+                <div className="basis-1/2">Image here</div>
             </div>
         </div>
     )
