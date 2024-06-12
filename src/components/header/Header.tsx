d627ec79f29be6b0b86cd8f91e81e1477451088f
Muhammad Irfan <naprirfan@gmail.com>
2024-06-12 11:21:35 +0800

    Add header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+    return (
+        <header className="h-45 bg-red">
+            <div className="text-center">Selamat datang di situs Pure Zen Herbs</div>
+        </header>
+    )
+}
