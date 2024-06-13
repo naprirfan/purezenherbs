2f51335d488a67a62b8fd153b8f965cf1b6508ad
Muhammad Irfan <naprirfan@gmail.com>
2024-06-13 20:12:17 +0800

    Add support for desktop

--- src/components/productsFeature/ProductsFeature.tsx
+++ src/components/productsFeature/ProductsFeature.tsx
@@ -1,14 +1,29 @@
 import db from '../../db.json';
 
 export const ProductsFeature = () => {
+    const title = 'Mengapa Pilih Pure Zen Herbs?';
+
     return (
         <div className="flex justify-center mb-6 lg:mb-8">
             <div className="container px-2">
-                <h2 className="text-4xl text-center mt-4 lg:my-6">Mengapa Pilih Pure Zen Herbs?</h2>
+                <h2 className="text-4xl text-center mt-4 lg:my-6">{title}</h2>
 
-                <img className="mt-6" src="https://placehold.co/400x400/png" alt="Mengapa Pilih Pure Zen Herbs?" />
+                <div className="lg:hidden">
+                    <img className="mt-6" src="https://placehold.co/400x400/png" alt={title} />
+                    <div className="grid grid-cols-2 gap-2 mt-4 lg:flex lg:flex-row lg:justify-between">
+                        {
+                            db.features.map((feature) => (
+                                <div key={feature.id} className="flex flex-col items-center">
+                                    <img className="w-3 h-3 m-2" src={`/assets/images/${feature.icon}`} alt={feature.title} />
+                                    <div className="uppercase text-center">{feature.title}</div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
 
-                <div className="grid grid-cols-2 gap-2 mt-4 lg:flex lg:flex-row lg:justify-between">
+                <div className="hidden lg:grid grid-cols-4 grid-rows-2">
+                    <div className="row-start-1 row-span-2 col-start-2 col-span-2 bg-contain bg-no-repeat bg-center min-h-[400px]" style={{ backgroundImage: 'url(https://placehold.co/400x400/png)'}} />
                     {
                         db.features.map((feature) => (
                             <div key={feature.id} className="flex flex-col items-center">
@@ -18,6 +33,7 @@ export const ProductsFeature = () => {
                         ))
                     }
                 </div>
+
             </div>
         </div>
     )
