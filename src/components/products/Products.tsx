a67483e7004e4ac1c1efa006114683d973303239
Muhammad Irfan <naprirfan@gmail.com>
2024-06-13 12:58:32 +0800

    init product list

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -2,17 +2,23 @@ import db from '../../db.json';
 
 export const Products = () => {
     return (
-        <div>
-            <h2 className="text-4xl text-center">Produk Kami</h2>
+        <div className="flex justify-center mb-4">
+            <div className="container px-2">
+                <h2 className="text-4xl text-center my-4">Produk Kami</h2>
 
-            <div className="flex">
-                {
-                    db.products.map((product) => (
-                        <div>
-                            {product.name}
-                        </div>
-                    ))
-                }
+                <div className="flex flex-row justify-between">
+                    {
+                        db.products.map((product) => (
+                            <div className="relative border">
+                                <img src={product.image} alt={product.name} />
+                                <div className="absolute top-2 left-2">
+                                    {product.name}
+                                </div>
+                                <a href="#" className="bg-charcoal w-full block text-white text-center py-1 block absolute top-0">Lihat Produk</a>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
