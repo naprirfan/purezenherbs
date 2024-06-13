0e85040669eb1ac58642694e27346cd27da755e9
Muhammad Irfan <naprirfan@gmail.com>
2024-06-13 14:03:45 +0800

    add responsive product

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,16 @@
 export const Hero = () => {
     return (
-        <div className="bg-gray h-[750px] w-full border-b flex justify-center">
+        <div className="bg-gray w-full border-b flex justify-center py-4">
             <div className="container px-2 lg:flex lg:flex-row">
                 <div className="flex items-center lg:basis-1/2">
                     <div>
-                        <h1 className="text-6xl p-10 text-bold">Where Traditional Wisdom Meets Convenience</h1>
-                        <button className="text-white border-2 bg-charcoal px-4 py-2 mt-4">Belanja Sekarang</button>
+                        <h1 className="text-5xl lg:text-6xl mb-3 text-bold">Where Traditional Wisdom Meets Convenience</h1>
+                        <button className="text-white bg-charcoal px-4 py-2">Belanja Sekarang</button>
                     </div>
                 </div>
-                <div className="lg:basis-1/2">Image here</div>
+                <div className="mt-3 lg:basis-1/2 lg:mt-0">
+                    <img className="block" src="https://placehold.co/800x600/png" alt="placeholder" />
+                </div>
             </div>
         </div>
     )
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -6,18 +6,18 @@ export const Products = () => {
             <div className="container px-2">
                 <h2 className="text-4xl text-center my-4">Produk Kami</h2>
 
-                <div className="flex flex-row justify-between">
+                <div className="grid grid-cols-2 gap-2 lg:grid-cols-4 lg:gap-4">
                     {
                         db.products.map((product) => (
-                            <div className="relative border">
-                                <a href={`/product/${product.id}/${product.name}`}>
-                                    <img src={product.image} alt={product.name} />
+                            <a className="block" href={`/product/${product.id}/${product.name}`}>
+                                <div className="relative border">
+                                    <img className="object-cover center" src={product.image} alt={product.name} />
                                     <div className="absolute top-2 left-2">
                                         {product.name}
                                     </div>
                                     <div className="bg-charcoal w-full block text-white text-center py-1">Lihat Produk</div>
-                                </a>
-                            </div>
+                                </div>
+                            </a>
                         ))
                     }
                 </div>
