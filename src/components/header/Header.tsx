cdc4f44384ba471982876a36cfaed931a8045970
Muhammad Irfan <naprirfan@gmail.com>
2024-06-14 19:17:55 +0800

    Make header responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,14 @@ export const Header = () => {
   return (
     <header className="w-full">
       <section className="bg-charcoal text-white text-center p-1">Selamat datang di situs Pure Zen Herbs</section>
-      <section className="h-12 border-b flex justify-center bg-gray">
-        <div className="container px-2 flex justify-between items-center">
-            <div>Shop | About Us</div>
-            <div>Logo</div>
-            <div>Language Control</div>
+      <section className="h-8 border-b flex justify-center bg-gray lg:h-12">
+        <div className="container px-2 justify-between items-center hidden lg:flex">
+          <div>Shop | About Us</div>
+          <div>Logo</div>
+          <div>Language Control</div>
+        </div>
+        <div className="container px-2 lg:hidden flex items-center">
+          <div>Logo</div>
         </div>
       </section>
     </header>
