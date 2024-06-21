6503c570bdba541686078033b9ea13c08a7236f4
Muhammad Irfan <naprirfan@gmail.com>
2024-06-21 20:48:08 +0800

    Fix hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,9 +3,9 @@ export const Hero = () => {
         <div className="bg-gray w-full border-b flex justify-center py-4">
             <div className="container px-2 lg:flex lg:flex-row">
                 <div className="flex items-center lg:basis-1/2">
-                    <div>
-                        <h1 className="mb-3 text-5xl text-bold text-center lg:text-left lg:text-6xl">Where Traditional Wisdom Meets Convenience</h1>
-                        <button className="text-white mx-auto block bg-charcoal px-4 py-2 lg:mx-0">Belanja Sekarang</button>
+                    <div className="flex flex-col items-center">
+                        <h1 className="mb-3 text-5xl text-bold text-center lg:text-left lg:text-6xl lg:pb-3">Where Traditional Wisdom Meets Convenience</h1>
+                        <a href="#" className="text-white uppercase text-center bg-charcoal px-4 py-2">Belanja Sekarang</a>
                     </div>
                 </div>
                 <div className="mt-3 lg:basis-1/2 lg:mt-0">
